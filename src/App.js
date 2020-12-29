2da93d5a71005e2d62641226237b3179119918e7
Venkata-Santhosh <santhosh.piduri@gmail.com>
2020-12-29 12:49:39 -0500

    Landing page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Santhosh, How are you Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="info-container">
+        <h1>Hi, I'm<span>Venkata Santhosh Piduri</span></h1>
     </div>
   );
 }
