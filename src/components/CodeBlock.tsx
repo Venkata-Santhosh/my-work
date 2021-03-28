1ce3ff9f34f55128057b35db61a3898a1af3d21f
Venkata-Santhosh <santhosh.piduri@gmail.com>
2021-03-27 21:21:33 -0400

    changes to color theme

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,21 +1,16 @@
 import React, { PureComponent } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {materialOceanic as CodeColor} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface CodeBlockPropType {
     value:string;
     language: string;
 }
 class CodeBlock extends PureComponent<CodeBlockPropType> {
-  
-  // static defaultProps = {
-    // language: null
-  // };
-
   render() {
     const { language, value } = this.props;
     return (
-      <SyntaxHighlighter language={language} style={dark}>
+      <SyntaxHighlighter language={language} style={CodeColor}>
         {value}
       </SyntaxHighlighter>
     );
